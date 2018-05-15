36b046f178d0a1d28e244ddd4d9060e15394505d
Ali Muazzam <muazzama@ducky.informatik.privat>
2018-05-16 00:04:02 +0200

    working copy

--- network.py
+++ network.py
@@ -87,7 +87,7 @@ def _predict_flow(inp):
 
     output = tf.layers.conv2d(
         inputs=tmp,
-        filters=3,
+        filters=2,
         kernel_size=3,
         strides=1,
         padding='same',
@@ -128,13 +128,16 @@ def _refine(inp, num_outputs, upsampled_prediction=None, features_direct=None,na
 
 
     bn1 = tf.contrib.layers.batch_norm(upsampled_features, 
-                                        is_training=is_train, 
+                                        is_training=True, 
                                         epsilon=1e-5, 
                                         decay = 0.9,  
                                         updates_collections=None, 
                                         scope='bn1')
 
-    act1 = tf.nn.relu(bn1, name='act1')
+    if num_outputs <= 3:
+        act1 = tf.nn.tanh(bn1, name='act1')
+    else:
+        act1 = tf.nn.relu(bn1, name='act1')
 
 
 
@@ -167,15 +170,8 @@ def conv_down(input_image):
 
 def generator(input, random_dim, is_train, reuse=False,image_pair=None):
 
-    conv5,conv4,conv3,conv2,conv1,conv0 = conv_down(image_pair)
+    conv5_downsample,conv4_downsample,conv3_downsample,conv2_downsample,conv1_downsample,conv0_downsample = conv_down(image_pair)
 
-    print(conv5)
-    print(conv4)
-    print(conv3)
-    print(conv2)
-    print(conv1)
-    print(conv0)
-    print('done')
 
     c4, c8, c16, c32, c64 = 512, 256, 128, 64, 32 # channel num
     s4 = 7
@@ -195,54 +191,97 @@ def generator(input, random_dim, is_train, reuse=False,image_pair=None):
         conv1 = tf.reshape(flat_conv1, shape=[-1, s4, s42, c4], name='conv1')
         bn1 = tf.contrib.layers.batch_norm(conv1, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn1')
         act1 = tf.nn.relu(bn1, name='act1')
-        print(act1)
+
+        with tf.variable_scope('refine4'):
+            concat4 = _refine(
+                inp=act1,
+                num_outputs=512,
+                upsampled_prediction=None,
+                features_direct=conv4_downsample,
+                name='upconv1'
+            )
 
         # (1, 14, 24, 256)
         # 8*8*256
         #Convolution, bias, activation, repeat! 
-        conv2 = tf.layers.conv2d_transpose(act1, c8, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
-                                           kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
-                                           name='conv2')
-        bn2 = tf.contrib.layers.batch_norm(conv2, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn2')
-        act2 = tf.nn.relu(bn2, name='act2')
-        print(act2)
+        # conv2 = tf.layers.conv2d_transpose(act1, c8, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+        #                                    kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+        #                                    name='conv2')
+        # bn2 = tf.contrib.layers.batch_norm(conv2, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn2')
+        # act2 = tf.nn.relu(bn2, name='act2')
 
+        with tf.variable_scope('refine3'):
+            concat3 = _refine(
+                inp=concat4,
+                num_outputs=256,
+                upsampled_prediction=None,
+                features_direct=conv3_downsample,
+                name='upconv2'
+            )
+
+        with tf.variable_scope('refine2'):
+            concat2 = _refine(
+                inp=concat3,
+                num_outputs=128,
+                upsampled_prediction=None,
+                features_direct=conv2_downsample,
+                name='upconv2'
+            )
+
+        with tf.variable_scope('refine1'):
+            concat1 = _refine(
+                inp=concat2,
+                num_outputs=64,
+                upsampled_prediction=None,
+                features_direct=conv1_downsample,
+                name='upconv2'
+            )
+
+        with tf.variable_scope('refine0'):
+            concat0 = _refine(
+                inp=concat1,
+                num_outputs=2,
+                upsampled_prediction=None,
+                features_direct=conv0_downsample,
+                name='upconv2'
+            )
+
+        prediction = _predict_flow(concat0)
         # (1, 28, 48, 128)
         # 16*16*128
-        conv3 = tf.layers.conv2d_transpose(act2, c16, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
-                                           kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
-                                           name='conv3')
-        bn3 = tf.contrib.layers.batch_norm(conv3, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn3')
-        act3 = tf.nn.relu(bn3, name='act3')
-        print(act3)
+        # conv3 = tf.layers.conv2d_transpose(act2, c16, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+        #                                    kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+        #                                    name='conv3')
+        # bn3 = tf.contrib.layers.batch_norm(conv3, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn3')
+        # act3 = tf.nn.relu(bn3, name='act3')
+        # print(act3)
 
-        # (1, 56, 96, 64)
-        # 32*32*64
-        conv4 = tf.layers.conv2d_transpose(act3, c32, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
-                                           kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
-                                           name='conv4')
-        bn4 = tf.contrib.layers.batch_norm(conv4, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn4')
-        act4 = tf.nn.relu(bn4, name='act4')
-        print(act4)
+        # # (1, 56, 96, 64)
+        # # 32*32*64
+        # conv4 = tf.layers.conv2d_transpose(act3, c32, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+        #                                    kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+        #                                    name='conv4')
+        # bn4 = tf.contrib.layers.batch_norm(conv4, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn4')
+        # act4 = tf.nn.relu(bn4, name='act4')
+        # print(act4)
 
-        # (1, 112, 192, 32)
-        # 64*64*32
-        conv5 = tf.layers.conv2d_transpose(act4, c64, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
-                                           kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
-                                           name='conv5')
-        bn5 = tf.contrib.layers.batch_norm(conv5, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn5')
-        act5 = tf.nn.relu(bn5, name='act5')
-        print(act5)
+        # # (1, 112, 192, 32)
+        # # 64*64*32
+        # conv5 = tf.layers.conv2d_transpose(act4, c64, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+        #                                    kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+        #                                    name='conv5')
+        # bn5 = tf.contrib.layers.batch_norm(conv5, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn5')
+        # act5 = tf.nn.relu(bn5, name='act5')
+        # print(act5)
         
         # (1, 224, 384, 2)
         #128*128*3
-        conv6 = tf.layers.conv2d_transpose(act5, output_dim, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
-                                           kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
-                                           name='conv6')
+        # conv6 = tf.layers.conv2d_transpose(act5, output_dim, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+        #                                    kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+        #                                    name='conv6')
         # bn6 = tf.contrib.layers.batch_norm(conv6, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn6')
-        act6 = tf.nn.tanh(conv6, name='act6')
-        print(act6)
-        return act6
+        print(prediction)
+        return prediction
 # def generator(image_pair, random_dim, is_train, reuse=False):
 
 #     with tf.variable_scope('generator'):
